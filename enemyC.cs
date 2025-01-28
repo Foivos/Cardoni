c7a3afb3072d1f72f9e1bd885fac6b48f7513bb5
gregoryKar <135746855+gregoryKar@users.noreply.github.com>
2025-01-28 22:49:59 +0200

    first night

--- enemyC.cs
+++ enemyC.cs
@@ -3,12 +3,13 @@ using System;
 
 public partial class enemyC : RigidBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+
+	public new virtual void _Ready()
 	{
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 	public override void _Process(double delta)
 	{
 		//QueueFree();
@@ -21,7 +22,7 @@ public partial class enemyC : RigidBody2D
 		GD.Print("Screen exited DIE");
 		QueueFree();
 	}
-	
+
 
 
 
--- spwningC.cs
+++ spwningC.cs
@@ -6,23 +6,26 @@ public partial class spwningC : Node
 
 
 	[Export] public PackedScene enemyPreffab { get; set; }
+	[Export] float spawnX;
+	[Export] float enemySpeed;
 
 
-	public void NewGame()
-	{
-		// _score = 0;
+	// public void NewGame()
+	// {
+	// _score = 0;
 
-		// var player = GetNode<Player>("Player");
-		// var startPosition = GetNode<Marker2D>("StartPosition");
-		// player.Start(startPosition.Position);
+	// var player = GetNode<Player>("Player");
+	// var startPosition = GetNode<Marker2D>("StartPosition");
+	// player.Start(startPosition.Position);
 
-		//GetNode<Timer>("StartTimer").Start();
-	}
+	//GetNode<Timer>("StartTimer").Start();
+	//}
+	// GetNode<Timer>("MobTimer").Start();
+	// GetNode<Timer>("ScoreTimer").Start();
 
 	private void _on_spawn_timer_timeout()
 	{
-		// GetNode<Timer>("MobTimer").Start();
-		// GetNode<Timer>("ScoreTimer").Start();
+
 
 		spawnEnemy();
 		GD.Print("spawned");
@@ -47,16 +50,19 @@ public partial class spwningC : Node
 
 		// Set the mob's position to a random location.
 		//mob.Position = mobSpawnLocation.Position;
-		mob.Position = new Vector2(0, 3);
+		mob.Position = new Vector2(spawnX, 50);
 
 		// Add some randomness to the direction.
 		//direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
-		float direction = GD.RandRange(-45, 45);
-		mob.Rotation = direction;
+		//float direction = GD.RandRange(-45, 45);
+		//mob.Rotation = direction;
 
 		// Choose the velocity.
-		var velocity = new Vector2((float)GD.RandRange(2.0, 2.0), 0);
-		mob.LinearVelocity = velocity.Rotated(direction);
+		//var velocity = new Vector2(0, -enemySpeed);//(float)GD.RandRange(2.0, 2.0)
+		mob.LinearVelocity = new Vector2(0, -enemySpeed);
+		mob.GravityScale = 0;
+	
+	
 
 		// Spawn the mob by adding it to the Main scene.
 		AddChild(mob);
