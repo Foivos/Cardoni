c4872df35a81b255b5ae1e1a915882bb20d8f13d
gregoryKar <135746855+gregoryKar@users.noreply.github.com>
2025-01-30 23:10:03 +0200

    testing shooting bullets / selection that follows mouse in grid

---
+++ gregFolder/testBullet.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class testBullet : RigidBody2D
+{
+
+
+
+
+
+	private void OnBodyEntered(Node body)
+	{
+
+
+		if (body.IsInGroup("enemies") == false) return;
+
+		var enemy = body.GetNodeOrNull<enemyC>("enemyC");
+		if (enemy == null) return;
+
+		enemy.QueueFree();
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
